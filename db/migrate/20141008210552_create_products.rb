5a3c502a752c26e6680282ac8946501c520d1ea6
Robert Elroy <relroy5499@gmail.com>
2014-10-26 15:02:43 -0500

    migrations surgery

--- db/migrate/20141008210552_create_products.rb
+++ db/migrate/20141008210552_create_products.rb
@@ -4,7 +4,7 @@ class CreateProducts < ActiveRecord::Migration
       t.string :name
       t.string :department
       t.string :isle
-      t.string :price
+      t.decimal :price, precision: 7, scale: 2
       t.string :brand
       t.string :description
 
